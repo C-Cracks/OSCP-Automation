2d6339bf8939d0b2140ee732a47a750499f048f8
C-Cracks <56517590+C-Cracks@users.noreply.github.com>
2020-04-02 22:35:15 +0100

    Took out unneccessary check for HTTPS
    Made the orig unnecessarily check for the presence of HTTPS before using the --insecure flag

--- test-methods.sh
+++ test-methods.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/bash
 
 methods=("TRACE" "GET" "PUT" "HEAD" "DELETE" "CONNECT" "OPTIONS" "invalid")
@@ -5,12 +6,7 @@ methods=("TRACE" "GET" "PUT" "HEAD" "DELETE" "CONNECT" "OPTIONS" "invalid")
 echo -e "$( tput bold )C-Cracks\nHTTP Methods Testing$( tput sgr0 )"
 
 for m in "${methods[@]}";do
-	if [[ $( echo "$1" | grep https ) ]] ; then
-		if [[ $( echo "$m" | grep "HEAD" ) ]]; then figlet "${m} Request" && curl --insecure --head "$1"
-		else figlet "${m} Request" && curl --insecure -X "${m}" "$1" ; fi
-	elif [[ $( echo "$1" | grep http ) ]] ; then
-		if [[ $( echo "$m" | grep "HEAD" ) ]]; then figlet "${m} Request" && curl --head "$1"
-		else figlet "${m} Request" && curl -X "${m}" "$1" ; fi
-	fi
+	if [[ $( echo "$m" | grep "HEAD" ) ]]; then figlet "${m} Request" && curl --insecure --head "$1"
+	else figlet "${m} Request" && curl --insecure -X "${m}" "$1" ; fi
 done
 exit 0
