984e61889cee38ab495a541a8044a5962dc71b57
C-Cracks <56517590+C-Cracks@users.noreply.github.com>
2020-06-23 23:16:15 +0100

    Yet more changes...
    Keep an eye out for changes. :)

--- enum.sh
+++ enum.sh
@@ -11,19 +11,19 @@ ip=$1 && echo -e "Target: ${ip}\nCommencing with nmap vulners scan..."
 
 # perform Nmap scan on all ports using NSE script vulners
 # Zenity creates alert boxes- removes the need to keep checking the terminal for output
-nmap -oN ./nmap-scan-results.txt --script nmap-vulners -sV ${ip} -p-  > /dev/null 2>&1 && zenity --info --text="Nmap Scan On ${ip} Complete. Results saved to nmap-scan-results.txt."
+nmap -oN ./nmap-scan-results.txt -T4 -sV ${ip} -p-  > /dev/null 2>&1 && zenity --info --text="Nmap Scan On ${ip} Complete. Results saved to nmap-scan-results.txt."
 cat ./nmap-scan-results.txt 
 
 # collect relevant ports and place into variables for use later
 # if more than 1 port returned, append to array, else continue with orig execution
-http_p=( `cat ./nmap-scan-results.txt | grep "http" | grep -v "ssl" | cut -d'/' -f 1 | grep -v [A-Za-z] || echo "HTTP not found."` )
+http_p=( `cat ./nmap-scan-results.txt | grep "http" | grep -v "ssl" | grep -v "over" | grep -v "HTTPAPI" | cut -d'/' -f 1 | grep -v [A-Za-z] || echo "HTTP not found."` )
 https_p=( `cat ./nmap-scan-results.txt | grep "ssl/http" || echo "HTTPS not found." | cut -d'/' -f 1` )
 
 ssh_p=$( cat ./nmap-scan-results.txt | grep "ssh" || echo "SSH not found." | cut -d'/' -f 1  ) 
 ftp_p=$( cat ./nmap-scan-results.txt | grep "ftp" || echo "FTP not found." | cut -d'/' -f 1 ) 
 
 # run enum4linux against target if target is linux
-if [[ $( cat nmap-scan-results.txt | grep "smb" ) ]] ; then echo -e "\nRunning enum4linux..." ; enum4linux "${ip}" > linux-enum.txt ; cat linux-enum.txt ; fi
+if [[ $( cat nmap-scan-results.txt | grep -E -- "smb|windows" ) ]] ; then echo -e "\nRunning enum4linux..." ; enum4linux "${ip}" > linux-enum.txt ; cat linux-enum.txt ; fi
 
 # perform wfuzz scans
 if [[ $( echo "${http_p[@]}" | grep -v "not found" ) ]] && [[ $( echo "${https_p[@]}" | grep -v "not found" ) ]] ; then 
@@ -94,14 +94,14 @@ echo "Initial enumeration complete" && ls -al
 
 open_ps=$( cat ./nmap-scan-results.txt | grep "open" ) ; resp=$( cat ./wfuzz.txt | grep "  200" ) 
 echo -e "\e[33m\e[1mRESULTS:\e[0m\e[33m\e[0m"
-echo -e "Open Ports:\n${open_ps}" ; echo -e "\nFiles returning 200 response (see wfuzz.txt if unsure on site.):\n${resp}\n"
+echo -e "Open Ports:\n${open_ps}" 
+if [[ $( cat ./curl.txt | wc -l ) -lt 1000 ]] ; then echo -e "\nFiles returning 200 response (see wfuzz.txt if unsure on site.):\n${resp}\n" && if [[ $( cat ./curl.txt | grep -E -- "login|admin|portal|robots" ) ]] ; then echo -e "Interesting Files:\n$( cat ./curl.txt | grep -E -- 'login|admin|portal|robots' )\nSee wfuzz.txt for location of file." ; fi ; fi
 
 if [[ $( echo "${open_ps}" | grep "ftp" ) ]] ; then echo -e "FTPs present, anonymous login could be a thing...\n" ; fi
 if [[ $( echo "${open_ps}" | grep "smbd" ) ]] ; then echo -e "Samba File Share present...\n" ; fi
 if [[ $( echo "${open_ps}" | grep "doom" ) ]] ; then echo -e "\nUnknown service is present, check this with telnet..." ; fi
-if [[ $( cat ./curl.txt | grep -E -- "login|admin|portal|robots" ) ]] ; then echo -e "Interesting Files:\n$( cat ./curl.txt | grep -E -- 'login|admin|portal|robots' )\nSee wfuzz.txt for location of file." ; fi
 if [[ $( cat nmap-scan-results.txt | grep -E -- "smb|windows" ) ]] ; then 
-	users=$( cat linux-enum.txt | grep "(Local User)" ) 
+	users=$( cat linux-enum.txt | grep -E -- "user:[|Local User" ) 
 	echo -e "\nLocal users discovered by enum4linux:\n${users}" 
 	echo "Discovered shares:" ; echo $( cat linux-enum.txt | grep "Mapping: OK, Listing: OK" )
 fi
